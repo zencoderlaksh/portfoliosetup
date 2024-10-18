5ff1e102a1443915f8dac4e02150b3860d5ea073
zencoderlaksh <zencoderlaksh@gmail.com>
2024-10-18 10:09:41 +0530

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useSmoothScroll } from "./useSmoothScroll"; // Ensure this path points to where you saved the hook
 import AppRoutes from "./routes/AppRoutes";
+import "../src/styles/global.css";
 
 const App = () => {
-  // Apply the smooth scroll hook
-  useSmoothScroll();
-
   return (
     <div id="main-container" data-scroll-container>
       {/* Your App Routes */}
